e8081399b30882c79db9a930886045e50719a0ec
Shivam Rathore <shivam.rathore010@gmail.com>
2018-08-19 21:50:06 +0530

    update

--- install_protobuf.sh
+++ install_protobuf.sh
@@ -2,7 +2,6 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  echo "Downloading protobuf and caching."
   wget https://github.com/google/protobuf/releases/download/v3.6.1/protobuf-python-3.6.1.tar.gz
   tar -xzvf protobuf-python-3.6.1.tar.gz
   cd protobuf-3.6.1 && ./configure --prefix=$HOME/protobuf && make && make install
@@ -11,9 +10,9 @@ if [ ! -d "$HOME/protobuf/lib" ]; then
   cd python
   python setup.py build
   python setup.py test
-  cd ..
   pwd 
   ls -la 
+  echo "Downloading protobuf and caching."
 else
   echo "Using cached directory."
 fi
