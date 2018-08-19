4fe2ae9b5763b92a9b5bb14df3e90c346eb44f0d
Shivam Rathore <shivam.rathore010@gmail.com>
2018-08-19 23:05:26 +0530

    .travis.yml updated | caching protobuf

--- install_protobuf.sh
+++ install_protobuf.sh
@@ -3,17 +3,13 @@ set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/releases/download/v3.6.1/protobuf-python-3.6.1.tar.gz
-  ls -la
   tar -xzvf protobuf-python-3.6.1.tar.gz
-  ls -la
   pushd protobuf-3.6.1 && ./configure --prefix=$HOME/protobuf && make && sudo make install && popd
   pwd
   ls -la
   cd protobuf-3.6.1/python
   python setup.py build
   python setup.py test
-  pwd 
-  ls -la 
   echo "Downloading protobuf and caching."
 else
   echo "Using cached directory."
