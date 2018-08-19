6d483279fd70378e4a01179551a534abdf8bf730
Shivam Rathore <shivam.rathore010@gmail.com>
2018-08-19 22:08:51 +0530

    removing cache layer

--- install_protobuf.sh
+++ install_protobuf.sh
@@ -3,8 +3,10 @@ set -e
 # check to see if protobuf folder is empty
 if [ 1 ]; then
   wget https://github.com/google/protobuf/releases/download/v3.6.1/protobuf-python-3.6.1.tar.gz
+  ls -la
   tar -xzvf protobuf-python-3.6.1.tar.gz
-  cd protobuf-3.6.1 && ./configure --prefix=$HOME/protobuf && make && make install
+  ls -la
+  pushd protobuf-3.6.1 && ./configure --prefix=$HOME/protobuf && make && sudo make install && popd
   pwd
   ls -la
   cd python
