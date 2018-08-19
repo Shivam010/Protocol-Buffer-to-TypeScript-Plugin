f619869ba09d48eb1d5ae6aed145b353901d8b22
Shivam Rathore <shivam.rathore010@gmail.com>
2018-08-19 21:42:22 +0530

    output directory handled

--- examples/generate.sh
+++ examples/generate.sh
@@ -2,10 +2,14 @@
 local_path=$(pwd)
 
 generate() {
-    $HOME/protobuf/bin/protoc -I . -I ${local_path} ${local_path}/${1}*.proto --plugin=protoc-gen-custom=../tsPlugin.py --custom_out=${local_path}/new_outputs
+    $HOME/protobuf/bin/protoc -I ${local_path} ${local_path}/${1}*.proto --plugin=protoc-gen-custom=../tsPlugin.py --custom_out=${local_path}/new_outputs
 }
 
-mkdir new_outputs
+if [ ! -d "new_outputs" ]; then
+    mkdir new_outputs
+else 
+    rm -rf new_outputs/*
+fi
 for d in */; do
     generate ${d}
 done
