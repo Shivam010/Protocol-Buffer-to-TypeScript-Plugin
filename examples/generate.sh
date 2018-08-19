874dc949979b9e80e42aed76db719253161c5751
Shivam Rathore <shivam.rathore010@gmail.com>
2018-08-19 20:45:38 +0530

    making current directory as project directory

--- examples/generate.sh
+++ examples/generate.sh
@@ -2,7 +2,7 @@
 local_path=$(pwd)
 
 generate() {
-    $HOME/protobuf/bin/protoc -I /usr/local/include -I ${local_path} ${local_path}/${1}*.proto --plugin=protoc-gen-custom=../tsPlugin.py --custom_out=${local_path}/new_outputs
+    protoc -I . -I ${local_path} ${local_path}/${1}*.proto --plugin=protoc-gen-custom=../tsPlugin.py --custom_out=${local_path}/new_outputs
 }
 
 mkdir new_outputs
